3ada91501561e0575f0c2ef05d9127a9e59acbf4
AlexandreG83 <alexandre.guillamon83@gmail.com>
2025-11-27 11:57:22 +0000

    Add character, string and percent printing handlers
    - Implement print_char() using _putchar and va_arg
    - Implement print_string() with NULL safety fallback to (null)
    - Implement print_percent() with ignored va_list
    - These handlers integrate with _printf's format dispatching

---
+++ print_functions.c
@@ -0,0 +1,47 @@
+/**
+ * print_char - Prints a character
+ * @args: va_list containing the character
+ *
+ * Return: Number of characters printed (1)
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar(c));
+}
+
+/**
+ * print_string - Prints a string
+ * @args: va_list containing the string
+ *
+ * Return: Number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count]);
+		count++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_percent - Prints a percent sign
+ * @args: va_list (unused)
+ *
+ * Return: Number of characters printed (1)
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	return (_putchar('%'));
+}
